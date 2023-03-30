34a930a5aecd21f39dfed4df8fc0e9cc9b61678a
fdiana123 <123403220+fdiana123@users.noreply.github.com>
2023-03-30 10:24:23 +0300

    https://github.com/fdiana123/my-website3

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const hamburder = document.querySelector('.header .nav-bar .nav-list .hamburder');
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('header.container');
 
-hamburder .addEventListener('click',() => {
-    hamburder.classList.toggle('active');
+hamburger .addEventListener('click',() => {
+    hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position >250) {
+        header.style.backgraundColor = '#29323c';
+    } else{
+        header.style.backgraundColor = 'transparent';
+    }
+});
+
+menu_item.forEach(item=>{
+    item.addEventListener('click',()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
 });
